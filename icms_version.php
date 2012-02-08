4e3509f5decde26bed24c9307a6ed0d317f4f318
Simon <simon@isengard.biz>
2012-02-08 03:25:16 +0000

    Update publication date.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/news_ipf/trunk@23407 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- icms_version.php
+++ icms_version.php
@@ -34,7 +34,7 @@ $modversion = array(
 /**  Development information */
   'status_version'=> "1.1",
   'status'=> "Beta",
-  'date'=> "30/12/2011",
+  'date'=> "8/2/2012",
   'author_word'=> "This module is best used with the Sprockets utility module also installed.",
 
 /** Contributors */
