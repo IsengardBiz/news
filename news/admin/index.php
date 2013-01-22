5dc4de159d696b732e4457397bbb9ec5f0dcfcc2
Simon <simon@isengard.biz>
2013-01-22 02:31:14 +0000

    Filing V1.14 final.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/news_ipf/releases/1.14%20final@24846 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ news/admin/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Admin index page of the module
+*
+* Including the article page
+*
+* @copyright	Copyright Madfish (Simon Wilkinson) 2011
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		Madfish (Simon Wilkinson) <simon@isengard.biz>
+* @package		news
+* @version		$Id$
+*/
+
+header('location: article.php');
+exit;
